10d430df4c4feb25d2a5228334b269419db81f70
USER <USER@DESKTOP-LSL3HKK>
2017-11-26 18:49:53 +0530

    Chapter 1 almost

--- Exam_70-483/Program.cs
+++ Exam_70-483/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exam_70_483
 {
-    class Program
+    public class Program
     {
+        public static void Main()
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task read = Task.Run(() =>
+            {
+                foreach (string v in col.GetConsumingEnumerable())
+                    Console.WriteLine(v);
+            });
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s)) break;
+                    col.Add(s);
+                    
+                }
+            });
+            write.Wait();
+        }
     }
 }
